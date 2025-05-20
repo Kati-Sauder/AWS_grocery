9d1eefb83f445aef8bb3b341206868ec919c273e
Kati-Sauder <katarina.aguilerasauder@gmail.com>
2025-05-20 12:41:17 +0200

    modified main.tf file --> EC2 and RDS configuration.

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -2,28 +2,94 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  tags = {
+    Name = "main-vpc"
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.10.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "eu-central-1b"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_a_assoc" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b_assoc" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
+}
+
 resource "aws_security_group" "ec2_sg" {
-  name        = "allow_ssh"
-  description = "Allow SSH access"
-  vpc_id      = data.aws_vpc.default.id
+  name        = "ec2-ssh"
+  description = "Allow SSH from my IP"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.ssh_cidr]
+    cidr_blocks = ["94.139.31.185/32"]
   }
 
   egress {
@@ -34,21 +100,56 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   tags = {
-    Name = "TerraformSG"
+    Name = "ec2-sg"
   }
 }
 
-resource "aws_instance" "test_instance" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  subnet_id                   = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+resource "aws_instance" "my_ec2" {
+  ami                         = "ami-0b74f796d330ab49c"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
   associate_public_ip_address = true
-  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  key_name                    = "newest-keypair"
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
 
   tags = {
-    Name = "TerraformEC2"
+    Name = "EC2-Public"
   }
+}
+
+resource "aws_iam_role" "ec2_connect_role" {
+  name = "ec2-instance-connect-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "ec2_connect_policy" {
+  name = "ec2-instance-connect-policy"
+  role = aws_iam_role.ec2_connect_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = "ec2-instance-connect:SendSSHPublicKey",
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_connect_role.name
 }
 
 resource "aws_db_instance" "rds_instance" {
@@ -59,7 +160,7 @@ resource "aws_db_instance" "rds_instance" {
   username             = var.rds_username
   password             = var.rds_password
   db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot  = true
 
   tags = {
@@ -67,11 +168,52 @@ resource "aws_db_instance" "rds_instance" {
   }
 }
 
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-access"
+  description = "Allow Postgres from EC2"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
+  }
+}
+
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds-subnet-group"
-  subnet_ids = data.aws_subnets.default.ids
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
 
   tags = {
     Name = "RDSSubnetGroup"
   }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.20.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = false
 }
